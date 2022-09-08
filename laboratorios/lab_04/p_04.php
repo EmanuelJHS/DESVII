17dfea073f6529a91a38fce9ee02170eec30f18c
EmanuelJHS <emanuel0598@gmail.com>
2022-09-07 19:47:00 -0500

    enviar

---
+++ laboratorios/lab_04/p_04.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laboratorio 4.4</title>
+</head>
+<body>
+<p>Laboratorio 4.4: Manejo de Arreglos P2</p>
+    <?php 
+
+    ?>
+</body>
+</html>
